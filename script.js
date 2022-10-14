14f7921eb9b2a6e975e39084269aa0dff115cab5
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-10-14 17:08:33 +0200

    alert fix

--- script.js
+++ script.js
@@ -191,7 +191,6 @@ $(document).ready(function() {
       }),
       success: function(data) {
         console.log('Card created - ' + data.shortUrl);
-        alert('Card created - ' + data.shortUrl);
       }
     });
   }
